ac09542ae5c3272860c18d7e3934723add4c95f8
swarit <dholakia.swarit@gmail.com>
2022-03-03 17:10:43 -0500

    adding instructions

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -4,7 +4,41 @@ import './Instructions.css'
 const Instructions = () => {
     return (
         <div className='instructions'>
-            This is a template for the instructions yet to be written.
+
+            <h1>Game Play Instructions</h1>
+            
+            <spacer></spacer>
+
+            <h2>Overview:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+
+            <h2>Level 1:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+
+            <h2>Level 2:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+                
+            <h2>Level 3:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+    
+            <h2>Level 4:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+
+            <h2>Level 5:</h2>
+                <body></body>
+            
+            <spacer></spacer>
+
         </div>
     )
 }
