5566870f431111fa89c5ff5f11b395d6b14892f2
RyanH <rhayter2@uwo.ca>
2022-03-14 22:31:26 -0400

    comment

--- client/src/components/Levels/LevelTwo.js
+++ client/src/components/Levels/LevelTwo.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect}from 'react'
-
+//level 2 and 3 were suposed to be doen for release 1 but due to our negligence we have fallen behind on these, we intend
+//to make up for this over the next 2 weeks and hopefully be on schedule for sprint 4
 
 const LevelTwo = () => { 
     //states 
