3f2b9ebee7c4c25764a33c81737b2ab38ff7a16c
khan98 <45436561+khan98@users.noreply.github.com>
2022-03-29 19:21:10 -0400

    Create Scoreboard.js

---
+++ client/src/components/Scoreboard.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import './Scoreboard.css'
+
+const Scoreboard = () =>{
+  let lives = 3;
+  if(...){
+     lives -=;
+  }
+  if(lives == 0){
+    
+  }
+  return(
+    <div clasName = 'scoreboard'> 
+      <h1>
+    </div>
+  )
+}
+export default Scoreboard
