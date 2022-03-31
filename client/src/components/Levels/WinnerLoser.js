4038f3471c5c6e81ca1e70074b1c823a99c0dac7
swarit <dholakia.swarit@gmail.com>
2022-03-31 17:04:06 -0400

    test

--- client/src/components/Levels/WinnerLoser.js
+++ client/src/components/Levels/WinnerLoser.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import ReactAudioPlayer from 'react-audio-player';
 
 const WinSound = new Audio('/win.mp3');
 const LoseSound = new Audio('/lose.mp3');
-
-const PlayWin = () => {
-    WinSound.load();
-    WinSound.play();
-}
-
-const PlayLose = () => {
-    LoseSound.load();
-    LoseSound.play();
-}
 
 const WinnerLoser = ({lives, handleReset}) => {
 
@@ -20,24 +9,14 @@ const WinnerLoser = ({lives, handleReset}) => {
     const Winner = () => {
         return (<>
             <h1>Winner! You won the game with {lives} lives remaining.</h1>
-            {PlayWin}
             <button onClick = {handleReset}>Try Again</button>
-            <ReactAudioPlayer
-            src="win.mp3"
-            autoPlay
-            />
         </>);
     }
     
     const Loser = () => {
         return (<>
             <h1>Loser. You have no lives remaining</h1>
-            {PlayLose}
             <button onClick = {handleReset}>Try Again</button>
-            <ReactAudioPlayer
-            src="lose.mp3"
-            autoPlay
-            />
             </>);
     }
 
