6ca5079669d2be612eacccaeb73bcff5781711a1
khan98 <45436561+khan98@users.noreply.github.com>
2022-03-31 16:59:05 -0400

    Update Timer.js
    Testing to log seconds

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import './Timer.css'
 
 const Timer = () =>{
+
+ var diff;
+ var seconds; 
  //start recording time as soon as the level begins  
  window.onload = startTime;
   
@@ -11,9 +14,15 @@ const Timer = () =>{
  
  function endTime(){
    end = new Date();
-   var diff = end - start; //end result in ms
+   diff = end - start; //end result in ms
    diff /= 1000; //strip the ms
-   var seconds = Math.round(diff); //round to present a less messy number
+   seconds = Math.round(diff); //round to present a less messy number
  }
+ console.log(start.getSeconds());
+/// return (
+  //   <div className = 'timer'>
+    //     start.getTime();
+   //  </div>
+ //)
 }
 export default Timer
