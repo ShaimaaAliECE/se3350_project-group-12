45fb1cc57e318e3742068c27b9737a21085a0c28
khan98 <45436561+khan98@users.noreply.github.com>
2022-04-03 04:20:51 -0400

    Updates sources and conditions

--- client/src/components/Audio.js
+++ client/src/components/Audio.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react'
 const Audio = () =>{
+    let level;
+    let source;
+    if(level < 3){
+        source = "https://www.youtube.com/watch?v=Jb6Kwd8KWNg&ab_channel=Ther%C3%B3kMSM";
+    }else {
+        source = "https://www.youtube.com/watch?v=KsoxjylnQ68&ab_channel=kendowater";
+    }
     return(
         <audio autoplay="autoplay">
-            <source src = "https://www.youtube.com/watch?v=Jb6Kwd8KWNg&ab_channel=Ther%C3%B3kMSM"/>
+            <source src = source/>
         </audio>
     )
 }
