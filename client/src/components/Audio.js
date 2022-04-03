d653219fa46d4a5d3fab6eb17ad227010d52357b
khan98 <45436561+khan98@users.noreply.github.com>
2022-04-02 21:20:14 -0400

    Create Audio.js
    Background music (nice to have)

---
+++ client/src/components/Audio.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+const Audio = () =>{
+    return(
+        <audio autoplay="autoplay">
+            <source src = "https://www.youtube.com/watch?v=Jb6Kwd8KWNg&ab_channel=Ther%C3%B3kMSM"/>
+        </audio>
+    )
+}
+export default Audio
